8a8cb560ffa667fbb9085446b09b46b5bf3eb51e
Damian <domain2190@gmail.com>
2017-08-07 09:17:41 -0400

    testing new repository

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
-// seeddb();
 
 /////////////////////////////////////
 //Require Routes
