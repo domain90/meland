e46cc1900a984eb49cec14a5b6dd44d517217aaf
Damian <domain2190@gmail.com>
2017-08-08 18:45:11 -0400

    fb button

--- public/javascript/myjs.js
+++ public/javascript/myjs.js
@@ -166,13 +166,13 @@ $(function() {
 
 		var gagLink    = $(this).attr('href');
 		var gagTitle   = $('.head-title').find('h3').text();
-		var gagPicture = $('img').attr('src');
+		var gagPicture = $('.content-img').find('img').attr('src');
 		// $(this).parents(".content-img").sibligs("a").attr('href');
 		// $("a[href='http://www.google.com/']").attr('href', 'http://www.live.com/')
 		console.log(gagLink);
 		console.log(gagTitle);
 		console.log(gagPicture);
-		
+
 		FB.ui(
 		{
 			method: 'share',
